e33dfc9ff2ca57161285f9a5e15eafcb8a5d925c
TahourDev <72548356+tahourdev@users.noreply.github.com>
2024-02-24 13:10:51 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -69,6 +69,22 @@ export default function RootLayout({ children }) {
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathnam,
+          });
+            `,
+          }}
+        />
       </Head>
       <body
         className={clsx(
