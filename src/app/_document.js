13225dc09967af418f356e5743f4803144572fd1
TahourDev <72548356+tahourdev@users.noreply.github.com>
2024-02-24 11:10:46 +0700

    update

---
+++ src/app/_document.js
@@ -0,0 +1,35 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google Analytics tracking code */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-HQESLWFR6B"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-HQESLWFR6B');
+              `,
+            }}
+          />
+          {/* Other meta tags */}
+          {/* Include other meta tags here if needed */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
